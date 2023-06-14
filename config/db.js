fe0406c932572453aa5c800018cbfd9eeed741d5
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2023-06-14 16:00:38 -0400

    Implementing Environment and preparing for Deployment Dated June 13th 2023 04:00 PM

--- config/db.js
+++ config/db.js
@@ -5,13 +5,14 @@ Date: Thursday, October 18th, 2022 */
 
 //I will need to take care of my DB access password in the following weeks.
 
-let atlasDB = "mongodb+srv://meysamdbuser:gwqUiKclkg2K4utn@cluster005.ye5t026.mongodb.net/products";
+// let atlasDB = "mongodb+srv://meysamdbuser:gwqUiKclkg2K4utn@cluster005.ye5t026.mongodb.net/products";
+let config = require('./config');
 
 let mongoose = require('mongoose');
 
 module.exports = function(){
     //Connects to my DataBase:
-    mongoose.connect(atlasDB);
+    mongoose.connect(config.ATLASDB);
 
     let mongoDB = mongoose.connection;
     mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
