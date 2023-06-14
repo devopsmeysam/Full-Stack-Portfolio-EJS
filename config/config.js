97400959c02b3dac338bedb35c6fce1c850109f9
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2023-06-14 16:19:31 -0400

    Implements some edits on env and config.js Dated June 13th 2023 04:20 PM

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     "ATLASDB": process.env.ATLASDB,
     // "LOCALDB": "mongodb://localhost:27017/dbapp",
-    // "SECRETKEY": process.env.SECRETKEY
+    "SECRETKEY": process.env.SECRETKEY
 }
